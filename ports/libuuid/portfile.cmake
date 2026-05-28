48f474590b52a44427adc68dc876a3154343a4bb
Eugene Gershnik <gershnik@users.noreply.github.com>
2026-05-28 09:22:40 -0700

    Updating libuuid

--- ports/libuuid/portfile.cmake
+++ ports/libuuid/portfile.cmake
@@ -3,14 +3,14 @@ vcpkg_download_distfile(
     UTIL_LINUX_ARCHIVE 
     URLS https://github.com/util-linux/util-linux/archive/v${VERSION}.tar.gz
     FILENAME libuuid-${VERSION}.tar.gz
-    SHA512 747e988c6e35cb9254978aa8faeb2a0346d60b391cc44b1d150f9d6dc719cb2513294f9ddddef7c67a9b9dba00dabf52b69d34ffcebcab19c7a73ebb6bee8f66
+    SHA512 4846fea35fb10204a9a018a4e26994c9c5f4746e0c15fe0002aee1f2449d97be3b3fe432c6cdf3b10416eb9372488d2d4e5ac63927b086b3cd6e2dd18e56d169
 )
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO gershnik/libuuid-cmake
     REF "v${VERSION}"
-    SHA512 ea516b223c2fc4c789167481a8e3540e42452ca7b153787095dbd83a26d3637f063287f3719dcf2defa7847061c898fe73fd9cf136a947066774ce5068f4a15b
+    SHA512 c92b203f493197977d76c578b9687f63e3c83dc76a4eb454fb4722a9507587f98d7a780bfa2ddd43bbe40b7cb9159edd2ac09cf67434527a00c1414499511e0c
     HEAD_REF master
 )
 
