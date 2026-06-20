e51347eb8c5f4daa597ffd010223e6c7f825c078
Eugene Gershnik <gershnik@users.noreply.github.com>
2026-06-20 01:00:28 -0700

    Updating ptl

--- ports/ptl/portfile.cmake
+++ ports/ptl/portfile.cmake
@@ -4,18 +4,17 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO gershnik/ptl
     REF "v${VERSION}"
-    SHA512 c51f2a91ccbc1ca306744a53f6b9c507c8f32d21e26dffcf8c935d70dd307866be95ca77c3ac81a61e4848f4045090b8172d36436e36f7c1c3fa0fc07fb2a746
+    SHA512 4cf1ab5e56263be526c18c8afe9b420ee4440efc856ef009d0526e27e7f593037f8f65833f97d60d58d5abb56dfb8fb038a46d34b8e218c6f834af1b665b273f
     HEAD_REF master
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS 
-        "-DPTL_NO_TESTS=ON"
+        "-DBUILD_TESTING=OFF"
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/ptl PACKAGE_NAME ptl)
+vcpkg_cmake_config_fixup(PACKAGE_NAME ptl)
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
