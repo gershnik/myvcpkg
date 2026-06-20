b4156205ab88dfb76d9058f79138e6b1be17049b
Eugene Gershnik <gershnik@users.noreply.github.com>
2026-06-20 00:38:21 -0700

    Updating argum

--- ports/argum/portfile.cmake
+++ ports/argum/portfile.cmake
@@ -4,18 +4,17 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO gershnik/argum
     REF "v${VERSION}"
-    SHA512 e51ac022a4a8a5428692509f675a9ca8d12beb44e3f0fff59bfd281ddb42ea335d179fbfd1c83625726cce737e4fde52d19fdaf6d54a792f92506e0980146526
+    SHA512 7b549b200505f47a590d8f61d63bd9a852471302830bf965a36514af3e2f2df5b1444043182cd36fd63b65c798e983422d9334be20572849225f7aa013a42435
     HEAD_REF master
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS 
-        "-DARGUM_NO_TESTS=ON"
+        "-DBUILD_TESTING=OFF"
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/argum PACKAGE_NAME argum)
+vcpkg_cmake_config_fixup(PACKAGE_NAME argum)
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
